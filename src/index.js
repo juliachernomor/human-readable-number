79f290e2fe87be3f5e575188ac6d2957cde651e4
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-07-31 02:50:50 +0300

    feat: make some optimization2

--- src/index.js
+++ src/index.js
@@ -20,24 +20,3 @@ module.exports  = function toReadable (number) {
     return `${oneToNine[mass[0]]} hundred ${twentyToNinety[mass[1]]} ${oneToNine[mass[2]]}`
     }
 }
-
-// if (number>=100 && number<1000 && number%100 == 0) {return `${ones[mass[0]]} hundred`}
-// if (number === 110 || number === 210|| number=== 310 || number === 410|| number=== 510|| number=== 610||number=== 710||number=== 810||number=== 910) {
-//     return `${ones[mass[0]]} hundred ${ones[Number(mass[2])+10]}`
-//     }
-// if(mass.length === 3 && mass[2] == 0) {
-//     if(mass[2] == 0)
-//     return `${ones[mass[0]]} hundred ${tens[mass[1]]}`
-// }
-// if(mass.length === 3 && mass[2] == 0) {
-//     return `${ones[mass[0]]} hundred ${tens[mass[1]]}`
-// }
-// if (number>=110 && number<120 || number>210 && number<220 ||number>310 && number<320||number>410 && number<420||number>510 && number<520||number>610 && number<620||number>710 && number<720||number>810 && number<820||number>910 && number<920) {
-//     return `${ones[mass[0]]} hundred ${ones[Number(mass[2])+10]}`
-// }
-// if (number>100 && number<110 || number>200 && number<211 ||number>300 && number<311||number>400 && number<411||number>500 && number<511||number>600 && number<611||number>700 && number<711||number>800 && number<811||number>900 && number<911) {
-//     return `${ones[mass[0]]} hundred ${ones[mass[2]]}`
-// }
-// else {
-//     
-//     }
